c38e12989ba2ff97a8727909c0389ac533d0c505
Anjitha <anjithaap2017@gmail.com>
2022-06-09 19:53:12 +0530

    update notebooks

--- models/fcn_32.py
+++ models/fcn_32.py
@@ -1,41 +1,41 @@
 # Import Libraries
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Dropout, Activation, BatchNormalization
-from keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Dropout, Activation, BatchNormalization
+from tensorflow.keras.models import Model
 
 def FCN32():
 
     img_input = Input(shape=(512, 512, 3))
 
     x = Conv2D(64, 3, activation='relu', name='Block-1_Conv-1', padding='same') (img_input)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.1) (x)
     x = Conv2D(64, 3, activation='relu', name='Block-1_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.1) (x)
     x = MaxPooling2D(2, strides=2, name='P-1') (x)
 
     # Block 2
     x = Conv2D(128, 3, activation='relu', name='Block-2_Conv-1', padding='same') (x)
     x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = Conv2D(128, 3, activation='relu', name='Block-2_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = MaxPooling2D(2, strides=2, name='P-2') (x)
 
     # Block 3
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-1', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = MaxPooling2D(2, strides=2, name='P-3') (x)
     
 
     # Block 4
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-1', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = MaxPooling2D(2, strides=2, name='P-4') (x)
     
 
@@ -45,7 +45,7 @@ def FCN32():
     x = Conv2D(512, 3, activation='relu', name='Block-5_Conv-2', padding='same') (x)
     x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-5_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = MaxPooling2D(2, strides=2, name='P-5') (x)
 
 
@@ -62,7 +62,7 @@ def FCN32():
     x = Conv2DTranspose( 32, kernel_size=2, kernel_initializer='he_normal', name='Upsample_32x', strides=2) (x)
 
     x = Conv2D(1, 1, kernel_initializer='he_normal') (x)
-    x = Dropout(0.5) (x)
+    x = Dropout(0.1) (x)
 
     x = (Activation('sigmoid'))(x)
     model = Model(img_input, x)
--- models/fcn_8.py
+++ models/fcn_8.py
@@ -1,15 +1,15 @@
 # Import Libraries
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Dropout, Activation, BatchNormalization, add
-from keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Dropout, Activation, BatchNormalization, add
+from tensorflow.keras.models import Model
 
 def FCN8():
 
     img_input = Input(shape=(512, 512, 3))
 
     x = Conv2D(64, 3, activation='relu', name='Block-1_Conv-1', padding='same') (img_input)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.1) (x)
     x = Conv2D(64, 3, activation='relu', name='Block-1_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.1) (x)
     x = MaxPooling2D(2, strides=2, name='Pooling-1') (x)
     skip1 = x
     skip1 = Conv2D(1, 1, kernel_initializer='he_normal', name='S-1') (skip1)
@@ -18,18 +18,18 @@ def FCN8():
     x = Conv2D(128, 3, activation='relu', name='Block-2_Conv-1', padding='same') (x)
     x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = Conv2D(128, 3, activation='relu', name='Block-2_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = MaxPooling2D(2, strides=2, name='Pooling-2') (x)
     skip2 = x
     skip2 = Conv2D(1, 1, kernel_initializer='he_normal', name='S-2') (skip2)
 
     # Block 3
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-1', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(256, 3, activation='relu', name='Block-3_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = MaxPooling2D(2, strides=2, name='Pooling-3') (x)
     skip3 = x
     skip3 = Conv2D(1, 1, kernel_initializer='he_normal', name='S-3') (skip3)
@@ -37,11 +37,11 @@ def FCN8():
 
     # Block 4
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-1', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-2', padding='same') (x)
-    x = BatchNormalization() (x); x = Dropout(0.2) (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-4_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.3) (x)
     x = MaxPooling2D(2, strides=2, name='Pooling-4') (x)
     skip4 = x
     skip4 = Conv2D(1, 1, kernel_initializer='he_normal', name='S-4') (skip4)
@@ -53,7 +53,7 @@ def FCN8():
     x = Conv2D(512, 3, activation='relu', name='Block-5_Conv-2', padding='same') (x)
     x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = Conv2D(512, 3, activation='relu', name='Block-5_Conv-3', padding='same') (x)
-    x = BatchNormalization() (x)
+    x = BatchNormalization() (x); x = Dropout(0.2) (x)
     x = MaxPooling2D(2, strides=2, name='Pooling-5') (x)
 
 
@@ -75,7 +75,7 @@ def FCN8():
     x = Conv2DTranspose( 32, kernel_size=2, kernel_initializer='he_normal', name='Upsample_32x', strides=2) (x)
 
     x = Conv2D(1, 1, kernel_initializer='he_normal') (x)
-    x = Dropout(0.5) (x)
+    x = Dropout(0.1) (x)
 
     x = (Activation('sigmoid'))(x)
     model = Model(img_input, x)
