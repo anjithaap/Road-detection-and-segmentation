af6966267846c565745763013d388cad6b7db73d
Anjitha <anjithaap2017@gmail.com>
2022-06-14 18:21:38 +0530

    Update detetction script

--- predict.py
+++ predict.py
@@ -3,33 +3,29 @@
 
 # Import Libraries
 from loss_functions import soft_dice_loss, iou_coef
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 from skimage.io import imread
 import numpy as np
 
 
 # Load saved model
-model = load_model("./Trained_Model/Road_Model.h5", custom_objects={'soft_dice_loss': soft_dice_loss, 'iou_coef': iou_coef})
+model = load_model("./Trained_Model/Road_Model.h5", custom_objects={'soft_dice_loss': soft_dice_loss, 'iou_coef': iou_coef}) # Select random image from folder
 
 
-test_path = 'test_image.jpg'
-mask_path = 'test_image.jpg'
-test_img  = np.asarray([imread(test_path)])
+import os
+from random import sample
 
-f = plt.figure(figsize = (24, 20))
-f.add_subplot(1,3,1)
-plt.imshow(imread(test_path), cmap='gray')
-plt.title("Input Image")
+input = imread(f'Dataset/Images/{sample(os.listdir("Dataset/Images"),1)[0]}')
+test_data = np.asarray([input])
+output = model.predict(test_data, verbose=0)[0][:,:,0]
+
+for xi in range(len(output)):
+  for yi in range(len(output[xi])):
+    if output[xi][yi] > 0.1:
+      input[xi][yi] = [255, 255, 0]
+
+plt.rcParams["figure.figsize"] = (10,10)
+plt.imshow(input)
 plt.axis('off')
-
-f.add_subplot(1,3,2)
-plt.imshow(imread(mask_path), cmap='gray')
-plt.title("Ground Truth")
-plt.axis('off')
-
-f.add_subplot(1,3,3)
-plt.imshow(model.predict(test_img, verbose=1)[0][:,:,0], cmap='gray')
-plt.title("Predicted Image")
-plt.axis('off')
-
 plt.show()
